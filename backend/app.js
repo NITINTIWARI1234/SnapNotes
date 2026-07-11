98c48898a28545d53d79f4e822627c92154e18dc
NITINTIWARI1234 <ntiwarinitin320@gmail.com>
2026-07-11 18:38:33 +0530

    Connect React frontend with Express backend

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 // Home Route
 app.get("/", (req, res) => {
-  res.send("Welcome to SnapNotes API 🚀");
+ res.json({
+  message: "Welcome to SnapNotes API 🚀",
+});
 });
 
 module.exports = app;
