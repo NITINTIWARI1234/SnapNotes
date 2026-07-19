2776102e4fa05a72c39077cfc7c647385008ffc2
NITINTIWARI1234 <ntiwarinitin320@gmail.com>
2026-07-20 00:35:36 +0530

    Fix

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL
-    ? import.meta.env.VITE_API_URL
-    : "http://localhost:5000",
+    ? `${import.meta.env.VITE_API_URL}/api`
+    : "http://localhost:5000/api",
 });
 
 export default api;
