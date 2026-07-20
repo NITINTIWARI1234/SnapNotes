43e4c914ca4676a02a782c1ed81d6edbdec38cb8
NITINTIWARI1234 <ntiwarinitin320@gmail.com>
2026-07-20 12:59:45 +0530

    Use Deno JS runtime for yt-dlp on Render

--- backend/services/downloadService.js
+++ backend/services/downloadService.js
@@ -3,6 +3,7 @@ const path = require("path");
 const fs = require("fs");
 
 const COOKIE_PATH = path.join(__dirname, "../cookies.txt");
+const DENO_PATH = path.join(__dirname, "../deno/bin/deno");
 
 if (process.env.YT_COOKIES_B64 && !fs.existsSync(COOKIE_PATH)) {
     fs.writeFileSync(COOKIE_PATH, Buffer.from(process.env.YT_COOKIES_B64, "base64"));
@@ -10,14 +11,17 @@ if (process.env.YT_COOKIES_B64 && !fs.existsSync(COOKIE_PATH)) {
 
 async function downloadVideo(url) {
     const outputPath = path.join(__dirname, "../temp/video.%(ext)s");
-
     const options = { output: outputPath };
+
     if (fs.existsSync(COOKIE_PATH)) {
         options.cookies = COOKIE_PATH;
     }
+    if (fs.existsSync(DENO_PATH)) {
+        options.jsRuntimes = `deno:${DENO_PATH}`;
+        options.remoteComponents = "ejs:github";
+    }
 
     await ytdlp(url, options);
-
     return path.join(__dirname, "../temp");
 }
 
